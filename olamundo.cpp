8175932f1e63fae33a863bee0067cf77217b8c15
Alexffb <110848750+Alexffb32@users.noreply.github.com>
2023-04-26 11:51:08 +0100

    Add files via upload

---
+++ olamundo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and "};
+for (const string& word : msg)
+{
+    count << word << " ";
+}
+count << endl;
+}
